755501ec69e8a6cfdbce7bbbc9c9d31d2ac784ba
Judah <judahben149@gmail.com>
2023-08-14 01:23:53 +0100

    Initial commit

---
+++ shared/src/commonMain/kotlin/com/judahben149/kmmplayground/Greeting.kt
@@ -0,0 +1,9 @@
+package com.judahben149.kmmplayground
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/judahben149/kmmplayground/Platform.kt
@@ -0,0 +1,7 @@
+package com.judahben149.kmmplayground
+
+interface Platform {
+    val name: String
+}
+
+expect fun getPlatform(): Platform
